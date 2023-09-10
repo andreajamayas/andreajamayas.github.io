0537753466c05b42ef127b03f10a808239306946
Andrea Amaya <andreaj.amaya@gmail.com>
2023-09-10 17:28:59 -0500

    v-0-2-1
    Se agregó el manejo del DOM a la parte de reserva de sillas según la función seleccionada

--- Portafolio/Proyectos/Cine/script.js
+++ Portafolio/Proyectos/Cine/script.js
@@ -3,44 +3,48 @@
 
 let dbFunciones = [
     {
+        id : 0,
         pelicula : "Barbie",
         horaInicio : "5pm",
         horaFin : "7pm",
         cantSillas : 5,
         cantSillasDisponibles: 3,
         cantSillasOcupadas: 2,
-        arraySillas : [false, true, true, false, false],
-        arrayOcupantes : ["", "", "", "", ""]
+        arraySillas : [false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true],
+        arrayOcupantes : ["", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", ""]
     },
-    {
+    {   
+        id : 1,
         pelicula : "Avengers",
         horaInicio : "1pm",
         horaFin : "3pm",
         cantSillas : 5,
         cantSillasDisponibles: 5,
         cantSillasOcupadas: 0,
-        arraySillas : [false, false, false, false, false],
-        arrayOcupantes : ["", "", "", "", ""]
+        arraySillas : [false, false, false, false, false, false, false, false, false, false, false, false,false, false, false, false, false, false,false, false, false, false, false, false,false, false, false, false, false, false,false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false ],
+        arrayOcupantes : ["", "", "", "", "", "", "", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", ""]
     },
     {
+        id : 2,
         pelicula : "Flash",
         horaInicio : "3pm",
         horaFin : "5pm",
         cantSillas : 5,
         cantSillasDisponibles: 5,
         cantSillasOcupadas: 0,
-        arraySillas : [false, false, false, false, false],
-        arrayOcupantes : ["", "", "", "", ""]
+        arraySillas : [false, false, false, false, false, false, false, false, false, false, false, false,false, false, false, false, false, false,false, false, false, false, false, false,false, false, false, false, false, false,false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false ],
+        arrayOcupantes : ["", "", "", "", "", "", "", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", ""]
     }, 
-    {
+    {   
+        id : 3,
         pelicula : "Flash",
         horaInicio : "8pm",
         horaFin : "10pm",
         cantSillas : 5,
         cantSillasDisponibles: 0,
         cantSillasOcupadas: 0,
-        arraySillas : [false, false, false, false, false],
-        arrayOcupantes : ["", "", "", "", ""]
+        arraySillas : [false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true,false, true, true, false, false, true],
+        arrayOcupantes : ["", "", "", "", "", "", "", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", "","", "", "", "", "", ""]
     }
 
 ];
@@ -141,6 +145,8 @@ function renderizarFuncionesDisponibles(evento) {
         const miNodoFuncion = document.createElement('button');
         miNodoFuncion.classList.add('ov-btn-slide-left');
         miNodoFuncion.innerText = elm.horaInicio;
+        miNodoFuncion.setAttribute('marcador', elm.id);
+        console.log(elm.id);
         miNodoFuncion.addEventListener("click", mostrarSillas);
         miNodo.appendChild(miNodoFuncion);
     })
@@ -150,15 +156,94 @@ function renderizarFuncionesDisponibles(evento) {
     
 };
 
-let contenedorSillas = document.querySelector('.home-sillas');
-
+let mainSillas = document.querySelector('.home-sillas');
+let contenedorSillas = document.querySelector('.container');
+let filas = 6;
+let columnas = 8;
+// se renderiza la sala de cine con las sillas
 function mostrarSillas(evento) {
-    contenedorSillas.classList.remove('ocultar');
+    contenedorSillas.innerHTML = "";
+    let id = evento.target.getAttribute('marcador');
+    let cont= 0;
+    for (let i = 0; i < filas ; i++ ) {
+        const miNodoFila = document.createElement('div');
+        miNodoFila.classList.add('fila');
+        for (let j = 0; j < columnas ; j++) {
+            const miNodoSilla = document.createElement('div');
+            miNodoSilla.classList.add('silla');
+            miNodoSilla.setAttribute('marcador1', id);
+            miNodoSilla.setAttribute('marcador2', cont);
+            miNodoSilla.addEventListener('click', seleccionSilla);
+            dbFunciones[id].arrayOcupantes[cont] = "";
+            if (dbFunciones[id].arraySillas[cont] == true){
+                miNodoSilla.classList.add('ocupada');
+            };
+            miNodoFila.appendChild(miNodoSilla);
+            cont++;
+        }
+        contenedorSillas.appendChild(miNodoFila);
+    };  
+    mainSillas.classList.remove('ocultar');
+    borrarDatosDeReservaNoConfirmada();
+};
 
+// cuando se selecciona otra función se borran los datos de las sillas seleccionadas previamente para no generar conflictos en los datos y mezclar reservas de funciones
+function borrarDatosDeReservaNoConfirmada() {
+    sillasReservadas.splice(0);
+    costoTotal = 0;
+    textoSillasSeleccionadas.innerHTML = `Sillas releccionadas: N/A`;
+    textoTotalPagar.innerHTML = `Total a pagar: $0`;
+}
+
+// cuando se selecciona una silla se agrega al "carrito" y cambia de color para diferenciarla, pero si la silla ya estaba selecciona se quita el color y se quita del carrito, si la silla ya está ocupada no ocurre nada
+function seleccionSilla(evento) {
+    let id = evento.target.getAttribute('marcador1');
+    let silla = evento.target.getAttribute('marcador2');
+    console.log(dbFunciones[id].arraySillas[silla], dbFunciones[id].arrayOcupantes[silla]);
+    if (dbFunciones[id].arraySillas[silla] == false && dbFunciones[id].arrayOcupantes[silla] == '') {
+        evento.target.classList.add('seleccionada');
+        ocuparSilla(id, silla);
+    }
+    else if (dbFunciones[id].arraySillas[silla] == false && dbFunciones[id].arrayOcupantes[silla] != "") {
+        evento.target.classList.remove('seleccionada');
+        desocuparSilla(id, silla);
+    };
 };
 
 
+// esta función nos permite ocupar una silla cuando esta se encuentra disponible, y de igual manera, va sumando el precio de las boletas
+function ocuparSilla(numFuncion, numSilla) {
+        // arrFunciones[numFuncion].arraySillas[numSilla] = true;
+        // arrFunciones[numFuncion].cantSillasDisponibles--;
+        // arrFunciones[numFuncion].cantSillasOcupadas++;
+        arrFunciones[numFuncion].arrayOcupantes[numSilla] = 'usuario';
+        sillasReservadas.push(numSilla);
+        costoTotal += costoBoleta;
+        renderizarResumenSillas(sillasReservadas, costoTotal);
+};
 
+function desocuparSilla(numFuncion, numSilla) {
+    // arrFunciones[numFuncion].arraySillas[numSilla] = true;
+    // arrFunciones[numFuncion].cantSillasDisponibles--;
+    // arrFunciones[numFuncion].cantSillasOcupadas++;
+    arrFunciones[numFuncion].arrayOcupantes[numSilla] = '';
+    let indiceSillaBorrar = sillasReservadas.indexOf(numSilla);
+    sillasReservadas.splice(indiceSillaBorrar, 1);
+    costoTotal = costoTotal - costoBoleta;
+    renderizarResumenSillas(sillasReservadas, costoTotal);
+};
+
+let textoSillasSeleccionadas = document.querySelector('.sillas-seleccionadas');
+let textoTotalPagar = document.querySelector('.total-pagar');
+let textoSillasDisponibles = document.querySelector('.sillas-disponibles');
+let textoSillasOcupadas = document.querySelector('.sillas-ocupadas');
+
+function renderizarResumenSillas(sillasReservadas, costoTotal) {
+    textoSillasSeleccionadas.innerHTML = `Sillas releccionadas: ${sillasReservadas}`;
+    textoTotalPagar.innerHTML = `Total a pagar: $${costoTotal}`;
+};
+
+// se crea el evento que permite confirmar la compra de las boletas para la función seleccionada
 
 
 
@@ -175,8 +260,9 @@ let costoTotal = 0;
 trueOrFalse = true;
 
 class Funciones {
-    constructor({pelicula, horaInicio, horaFin, cantSillas, cantSillasDisponibles, cantSillasOcupadas, arraySillas, arrayOcupantes}){
+    constructor({id, pelicula, horaInicio, horaFin, cantSillas, cantSillasDisponibles, cantSillasOcupadas, arraySillas, arrayOcupantes}){
 
+        this.id = id;
         this.pelicula = pelicula;
         this.horaInicio = horaInicio;
         this.horaFin = horaFin;
@@ -195,145 +281,7 @@ function pushFunciones() {
   }
 
 pushFunciones();
-// nombre = prompt("Ingrese su nombre para continuar");
 
-// Se declaran los eventos necesarios para el funcionamiento del programa
-
-
-
-
-
-
-// esta es la funcion principal donde arranca el programa y decidimos que queremos hacer
-function initProgram() {
-    while (trueOrFalse) {
-        let selectSection = parseInt(prompt(
-            "¿Qué quieres hacer? \n 1. Ver cartelera \n 2. reservar Sillas \n 3. Salir "
-        ));
-        
-        switch (selectSection) {
-            case 1:
-                verCartelera();
-                break;
-            case 2:
-                reservarSillas();
-                break;
-            case 3:
-                trueOrFalse = false;
-                break;        
-        }
-    }     
-}
-
-// En verCartelera podemos ver el listado de pelicuals disponibles para ver
-function verCartelera() {
-    let cartelera = "";
-    let i=0;
-    arrPeliculas.forEach((elm) => {
-        cartelera += i + ". " + elm + "\n";
-        i++;
-    })
-    pelicula = prompt("esta es la cartelera:\n" + cartelera + "\n seleccione el número de la película de la que desea ver los horarios de las funciones");
-    filtrarFuncionesPelicula(arrPeliculas[pelicula]);
-}
-
-// esta función permite ver las funciones (horarios) disponibles de la pelicula seleccionada
-function filtrarFuncionesPelicula(pelicula) {
-    let funcionesDisponibles = arrFunciones.filter(fun => fun.pelicula === pelicula);
-    alert(funcionesDisponibles.map(elm => elm.horaInicio));
-}
-
-// esta funcion permite ver todas las funciones disponibles sin ninguna clase de filtro (es algo que tengo que arreglar para que todo quede con la anterior)
-function verFunciones() {
-    let funciones = "";
-    let i= 0;
-    arrFunciones.forEach((elm) => {
-        funciones += i +". " + elm.pelicula + "  Inicio:" + elm.horaInicio + "  Fin:" + elm.horaFin + "\n";
-        i++;
-    })
-    return funciones;   
-}
-
-// esta función nos permite ver el listado de sillas de la función seleccionada y si la silla se encuentra ocupada o disponible
-function verSillas(numFuncion) {
-    let sillas = "";
-    i=0;
-    arrFunciones[numFuncion].arraySillas.forEach((elm) => {
-        if (elm == false) {
-            sillas += i + ". disponible \n";
-        } else {
-            sillas += i + ". ocupada \n";
-        }
-        i++;
-    });
-    return sillas;
-}
-
-// esta función nos permite validar si la función de cine ya está llena o si aún quedan sillas disponibles
-function validarFuncionCompleta(numFuncion) {
-    let resultado = false;
-    if (arrFunciones[numFuncion].cantSillasOcupadas == arrFunciones[numFuncion].cantSillas) {
-        resultado = true;
-        alert("Esta función ya se encuentra llena")
-    }
-    return resultado;
-}
-
-// esta función nos permite ocupar una silla cuando esta se encuentra disponible, y de igual manera, va sumando el precio de las boletas
-function ocuparSilla(numFuncion, numSilla) {
-    if (arrFunciones[numFuncion].arraySillas[numSilla] == true) {
-        alert("Esta silla ya está ocupada");
-    } else {
-        arrFunciones[numFuncion].arraySillas[numSilla] = true;
-        arrFunciones[numFuncion].cantSillasDisponibles--;
-        arrFunciones[numFuncion].cantSillasOcupadas++;
-        arrFunciones[numFuncion].arrayOcupantes[numSilla] = nombre;
-        sillasReservadas.push(numSilla);
-        costoTotal += costoBoleta;
-
-        alert("Silla reservada con exito");
-    }
-
-}
-
-// esta función consulta al cliente si desea reservar otra silla
-function reservarOtraSilla(){
-    let resultado = false;
-    let reservar = prompt("¿Desea reservar otra silla?\n0.Si\n1.No");
-    if (reservar == 0) {
-        resultado = true;
-    }
-    return  resultado;
-}
-
-// esta función nos da un resumen de la compra, como lo es el nombre del cliente, pelicula seleccionada y sillas escogidas
-function verDetalleCompra(numFuncion, sillasReservadas) {
-    alert("Este es el detalle de su compra \nNombre: " + nombre +"\nFunción reservada: " + arrFunciones[numFuncion].pelicula + "\nSillas reservadas: " + sillasReservadas + "\nTotal a pagar: " + costoTotal);
-
-}
-
-// esta es la función principal que nos permite seleccionar que silla queremos reservar
-function reservarSillas() {
-    let reservarOtra = true;
-    let funciones = verFunciones();
-    let numFuncion = prompt("Seleccione el número de la función para la que desea reservar sillas \n" + funciones);
-
-    let funcionCompleta = validarFuncionCompleta(numFuncion);
-    
-    while (funcionCompleta == false && reservarOtra == true) {
-        let sillas = verSillas(numFuncion);
-
-        let numSilla = prompt("Selecciono la función: " + arrFunciones[numFuncion].pelicula + "\nEstas son las sillas para la función seleccionada, indique el número de silla que desea reservar: \n\n" + sillas);
-
-        ocuparSilla(numFuncion, numSilla);
-        
-        reservarOtra = reservarOtraSilla();
-        funcionCompleta = validarFuncionCompleta(numFuncion);
-    }
-
-    verDetalleCompra(numFuncion, sillasReservadas);
-    initProgram();
-}
 
 
 // initProgram();
